dc0ceb104c58085574bada1e674494718bed930d
Alain Anghelidi <alainanghelidi@gmail.com>
2022-12-01 08:11:14 +0100

    [ENH] improve solutions

--- 2022/day1/main.go
+++ 2022/day1/main.go
@@ -12,17 +12,18 @@ import (
 func main() {
 	b,_ := os.ReadFile("input.txt")
 	data := strings.Split(string(b),"\n\n")
-	sums := make([]int,len(data))
-	for _, d := range data {
+	n := len(data)
+	sums := make([]int,n)
+	for i, d := range data {
 		cals := strings.Split(strings.TrimSpace(d),"\n")
 		s := 0
 		for _, cal := range cals {
 			num, _ := strconv.Atoi(cal)
 			s+= num
 		}
-		sums = append(sums, s)
+		sums[i] =  s
 	}
 	sort.Ints(sums)
-	fmt.Println("Part 1:", sums[len(sums)-1])
-	fmt.Println("Part 2:", sums[len(sums)-1] + sums[len(sums)-2] + sums[len(sums)-3])
+	fmt.Println("Part 1:", sums[n-1])
+	fmt.Println("Part 2:", sums[n-1] + sums[n-2] + sums[n-3])
 }
