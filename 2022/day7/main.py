e15c4a11c5c94dbf4828b8ea32c9bfda80d279f5
Alain Anghelidi <alainanghelidi@gmail.com>
2022-12-07 22:46:19 +0100

    [FEAT] added day7

--- 2022/day7/main.py
+++ 2022/day7/main.py
@@ -1,47 +1,29 @@
 from collections import defaultdict, deque
 import re
 
-ans = 0
-d = defaultdict(int)
-current_dir = "/"
-FS = defaultdict(list)
+ans, d = 0, defaultdict(int)
 numbers = lambda x: [int(m) for m in re.findall(r"\d+", x)]
 
-with open("sample.txt") as f:
+with open("input.txt") as f:
     term_out = [cmd.strip().split() for cmd in f.read().strip().split("$") if cmd != ""]
 
+stack = deque()
 for cmd in term_out:
-    cached_dir = "/"
     match cmd:
+        case ["cd", ".."]:
+            stack.pop()
+            previous_dir = stack[-1]
         case ["cd", dest]:
-            if dest == "..":
-                current_dir = cached_dir
-            else:
-                FS[current_dir].append(dest)
-                current_dir = dest
+            stack.append(dest)
         case ["ls", *rest]:
-            d[current_dir] = sum(numbers("".join(rest)))
+            if cmd == cached_cmd:
+                continue
+            s_sizes = sum(numbers("".join(rest)))
+            p = ""
+            for node in stack:
+                p += node
+                d[p] += s_sizes
+    cached_cmd = cmd
 
-FS["/"].remove("/")
-
-
-def get_dir_size(k: str, d: dict, fs: dict) -> int:
-    Q = deque([k])
-    size = 0
-    visited = set()
-    while Q:
-        cd = Q.pop()
-        size += d[cd]
-        if cd in fs:
-            for sk in fs[cd]:
-                if sk in visited:
-                    continue
-                Q.append(sk)
-    return size
-
-
-total_size = 0
-for k in d:
-    if (size := get_dir_size(k, d, FS)) <= 100000:
-        total_size += size
-print(total_size)
+print(f"Part 1: {sum(v for v in d.values() if v <= 100000)}")
+print(f"Part 2: {min(v for v in d.values() if 70000000 - d['/'] +v > 30000000)}")
