edcbdd8d1fd50a052a59266ce5ebf8720fda3833
Alain Anghelidi <alainanghelidi@gmail.com>
2022-12-12 16:58:34 +0100

    [FEAT] added day12

--- 2022/day12/main.py
+++ 2022/day12/main.py
@@ -1,83 +1,57 @@
-from typing import Iterator
-import re
-from rich import print
 import heapq
-from dataclasses import dataclass,field
 
-# ord(value) - ord('a')
-#N8 = {
-#    "L": complex(-1, 0),
-#    "DUL": complex(-1, 1),
-#    "DDL": complex(-1, -1),
-#    "R": complex(1, 0),
-#    "DUR": complex(1, 1),
-#    "DDR": complex(1, -1),
-#    "D": complex(0, -1),
-#    "U": complex(0, 1),
-#}
-N4 = {
-    "L": complex(-1, 0),
-    "R": complex(1, 0),
-    "D": complex(0, 1),
-    "U": complex(0, -1),
-}
+with open("input.txt") as f:
+    data = f.read().strip().splitlines()
 
-
-def n4(pos: complex) -> Iterator[complex]:
-    global grid
-    for np in N4.values():
-        np = pos + np
-        if np in grid:
-            yield np
-
-@dataclass(order=True)
-class Position:
-    elevation: int
-    position: complex=field(compare=False)
-
-    def __post_init__(self) -> None:
-        self.elevation *= -1
-
-grid = {}
-key_positions = {}
-with open("sample.txt") as f:
-    for y, line in enumerate(f):
-        line = line.strip()
-        for x, value in enumerate(line):
-            if value == 'S' or value == 'E':
-                key_positions[value] = complex(x,y)
-                grid[complex(x, y)] = Position(ord(value),complex(x,y)) 
-            else:
-                grid[complex(x, y)] = Position(ord(value)-ord('a'),complex(x,y))
-
-ans = 0
-visited = set()
-pq = []
-heapq.heapify(pq)
-heapq.heappush(pq,grid[key_positions['S']])
-visited.add(key_positions['S'])
-
-while pq:
-    current_pos : Position = heapq.heappop(pq)
-    tmp_np = []
-    for np in n4(current_pos.position):
-        if np == key_positions['E']:
-            print(ans)
-            raise SystemExit()
-        if current_pos.position == key_positions['S']:
-            current_pos.elevation = ord('a') - ord('a')
-        if np not in visited:
-            if grid[np].elevation >= current_pos.elevation - 1:
-                tmp_np.append(grid[np])
-                visited.add(np)
-    breakpoint()
-    for np in tmp_np:
-        if np == (current_pos.position + N4["R"]) or np == (current_pos.position + N4["D"]):
-            heapq.heappush(pq,np)
+H, W, grid, key_positions = len(data), len(data[0]), [], {}
+for i in range(H):
+    row = [0] * W
+    for j in range(W):
+        if (value := data[i][j]) in ["S", "E"]:
+            row[j] = 0 if value == "S" else 25
+            key_positions[value] = (i, j)
         else:
-            heapq.heappush(pq,np)
-    ans += 1
+            row[j] = ord(value) - ord("a")
+    grid.append(row)
 
 
+def n4(i, j):
+    for dr, dc in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+        ni = i + dr
+        nj = j + dc
+        if 0 <= ni < H and 0 <= nj < W and grid[ni][nj] <= grid[i][j] + 1:
+            yield ni, nj
 
-print(ans)
+
+visited, start = set(), key_positions["S"]
+pq = [(0, start)]
+while pq:
+    steps, (i, j) = heapq.heappop(pq)
+    if (i, j) in visited:
+        continue
+    visited.add((i, j))
+    if (i, j) == key_positions["E"]:
+        print(f"Part 1: {steps}")
+        break
+    for ni, nj in n4(i, j):
+        heapq.heappush(pq, (steps + 1, (ni, nj)))
+
+all_a_pos = [(i, j) for i in range(H) for j in range(W) if grid[i][j] == 0]
+
+# Brute force
+all_steps = []
+for a_start in all_a_pos:
+    visited, start = set(), a_start
+    pq = [(0, start)]
+    while pq:
+        steps, (i, j) = heapq.heappop(pq)
+        if (i, j) in visited:
+            continue
+        visited.add((i, j))
+        if (i, j) == key_positions["E"]:
+            all_steps.append(steps)
+            break
+        for ni, nj in n4(i, j):
+            heapq.heappush(pq, (steps + 1, (ni, nj)))
+
+print(f"Part 2: {min(all_steps)}")
